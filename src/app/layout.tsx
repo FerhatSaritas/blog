711af388e8297a90f53370860a45fd731dad9717
Ferhat Saritas <ferhat.saritas@init.de>
2024-01-24 10:12:38 +0100

    Add Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Footer, Header } from "./_components/organism";
 import "../../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({
   children,
@@ -14,6 +15,7 @@ export default function RootLayout({
         <div className='flex flex-row w-full'>
           <div className='container py-4 min-h-screen'>{children}</div>
         </div>
+        <Analytics />
         <Footer />
       </body>
     </html>
